70fd6133561c9c8539ccd03082e7e7d3bc5d0382
devkoow <cwl64658@gmail.com>
2024-10-22 22:40:57 +0900

    [Type] bookmarkActions.ts 액션 함수 타입 명시

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -5,9 +5,9 @@ import { createServerSupabaseClient } from 'utils/supabase/server';
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
-  const code = searchParams.get('code');
+  const code: string | null = searchParams.get('code');
   // if "next" is in param, use it as the redirect URL
-  const next = searchParams.get('next') ?? '/';
+  const next: string = searchParams.get('next') ?? '/';
 
   if (code) {
     const supabase = await createServerSupabaseClient();
