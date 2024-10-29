596dcf4b19e90b9e1691343508094fb4df29aea8
devkoow <cwl64658@gmail.com>
2024-10-29 12:22:18 +0900

    [Fix] 카카오 로그인 리다이렉트 수정

--- utils/supabase/signinKakao.ts
+++ utils/supabase/signinKakao.ts
@@ -8,9 +8,7 @@ export const signInWithKakao = async () => {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'kakao',
     options: {
-      redirectTo: process.env.NEXT_PUBLIC_API_REQUEST_URI
-        ? `${process.env.NEXT_PUBLIC_API_REQUEST_URI}/auth/callback`
-        : 'http://localhost:3000/auth/callback',
+      redirectTo: `${process.env.NEXT_PUBLIC_API_REQUEST_URI}/auth/callback`,
     },
   });
 
