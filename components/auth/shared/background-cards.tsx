1f4b92ca23d675eae375f255d7c9dc8f192d355d
devkoow <cwl64658@gmail.com>
2024-12-16 19:18:00 +0900

    [Refactor] Auth bg의 카드를 li 태그 대신 Image로 변경

--- components/auth/shared/background-cards.tsx
+++ components/auth/shared/background-cards.tsx
@@ -1,14 +1,69 @@
+'use client';
+
+import Image from 'next/image';
+
 export default function AuthBackgroundCards() {
+  const imageSrc =
+    'https://vsemazasjbizehcambul.supabase.co/storage/v1/object/public/cafe%20masters/card_transparent.webp';
+
   return (
-    <ul className="circles z-0">
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>
+    <div className="circles">
+      <Image
+        src={imageSrc}
+        alt="background card"
+        width={104}
+        height={104}
+        priority
+      />
+      <Image
+        src={imageSrc}
+        alt="background card"
+        width={80}
+        height={80}
+        priority
+      />
+      <Image
+        src={imageSrc}
+        alt="background card"
+        width={128}
+        height={128}
+        priority
+      />
+      <Image
+        src={imageSrc}
+        alt="background card"
+        width={144}
+        height={144}
+        priority
+      />
+      <Image
+        src={imageSrc}
+        alt="background card"
+        width={24}
+        height={24}
+        priority
+      />
+      <Image
+        src={imageSrc}
+        alt="background card"
+        width={96}
+        height={112}
+        priority
+      />
+      <Image
+        src={imageSrc}
+        alt="background card"
+        width={56}
+        height={56}
+        priority
+      />
+      <Image
+        src={imageSrc}
+        alt="background card"
+        width={104}
+        height={104}
+        priority
+      />
+    </div>
   );
 }
