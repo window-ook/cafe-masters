5d6a2119a46561a6e2411f2e01f7ee22ddf8de9c
devkoow <cwl64658@gmail.com>
2025-01-12 21:34:36 +0900

    [Fix] sitemap.ts로 다시 제출

---
+++ app/sitemap.ts
@@ -0,0 +1,54 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://app.cafe-masters.co',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+      alternates: {
+        languages: {
+          ko: 'https://app.cafe-masters.co/ko',
+          en: 'https://app.cafe-masters.co/en',
+        },
+      },
+    },
+    {
+      url: 'https://app.cafe-masters.co/cafe/all',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8,
+      alternates: {
+        languages: {
+          ko: 'https://app.cafe-masters.co/ko',
+          en: 'https://app.cafe-masters.co/en',
+        },
+      },
+    },
+    {
+      url: 'https://app.cafe-masters.co/collected/all',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8,
+      alternates: {
+        languages: {
+          ko: 'https://app.cafe-masters.co/ko',
+          en: 'https://app.cafe-masters.co/en',
+        },
+      },
+    },
+    {
+      url: 'https://app.cafe-masters.co/bookmarked/all',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8,
+      alternates: {
+        languages: {
+          ko: 'https://app.cafe-masters.co/ko',
+          en: 'https://app.cafe-masters.co/en',
+        },
+      },
+    },
+  ];
+}
