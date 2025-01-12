ab6d09835e868c0df99137632d3153b600c30470
devkoow <cwl64658@gmail.com>
2025-01-12 20:19:29 +0900

    [Chore] next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://app.cafe-masters.co', // 사이트의 기본 URL
+  generateRobotsTxt: true,
+  sitemapSize: 5000, // 한 파일에 포함될 URL 수
+};
