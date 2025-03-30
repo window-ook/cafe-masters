c8f90f4c8683622a7113ea1d259060c2ae1e7d17
devkoow <cwl64658@gmail.com>
2025-03-30 20:04:50 +0900

    ESLint fix

--- components/layouts/sidebar/main/help.tsx
+++ components/layouts/sidebar/main/help.tsx
@@ -2,10 +2,12 @@ export default function Help() {
   const googleForm = 'https://forms.gle/7jEc8cKfELoKDFPb7';
 
   return (
-      <div className='flex flex-col gap-4'>
-          <article>
-              <p className='font-pretendard'>버그를 발견하셨다면 저에게 알려주세요!</p>
-          </article>
+    <div className="flex flex-col gap-4">
+      <article>
+        <p className="font-pretendard">
+          버그를 발견하셨다면 저에게 알려주세요!
+        </p>
+      </article>
       <button
         onClick={() => window.open(googleForm, '_blank')}
         className="px-4 py-2 rounded-md bg-main text-white hover:bg-opacity-80 transition duration-150 ease"
