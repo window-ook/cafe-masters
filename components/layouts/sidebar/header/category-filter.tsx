83f1f9a4dda5b80c6ecb17a07f620f23834eb0e8
devkoow <cwl64658@gmail.com>
2025-04-11 12:10:32 +0900

    [Feat] 카테고리 필터, 셀렉터에 초기화 버튼 추가. 카테고리 필터 펼치기/접기 추가

--- components/layouts/sidebar/header/category-filter.tsx
+++ components/layouts/sidebar/header/category-filter.tsx
@@ -3,9 +3,12 @@
 import { useEffect, useState } from 'react';
 import { useMapStore } from 'utils/store';
 import { categories } from '../sub-sidebar/shared/categories';
+import { RiResetLeftFill } from 'react-icons/ri';
+import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
 
 export default function CategoryFilter() {
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+  const [isExpanded, setIsExpanded] = useState(true);
 
   const { recommendedCafe, setFilteredRecommendedCafe } = useMapStore();
 
@@ -16,6 +19,10 @@ export default function CategoryFilter() {
         : [...prev, category],
     );
   };
+
+  const resetCategories = () => setSelectedCategories(() => []);
+
+  const toggleExpand = () => setIsExpanded(prev => !prev);
 
   useEffect(() => {
     const filteredRecommendedCafe = recommendedCafe.filter(cafe => {
@@ -37,21 +44,47 @@ export default function CategoryFilter() {
 
   return (
     <div className="pt-2 flex flex-wrap justify-center gap-2">
-      {categories.map(category => (
-        <a
-          key={category}
-          onClick={() => toggleCategories(category)}
-          className={`px-4 py-2 rounded-full border transition text-sm cursor-pointer
-            ${
-              selectedCategories.includes(category)
-                ? 'bg-main text-white border-main'
-                : 'bg-gray-50 text-gray-700 border-gray-300 hover:bg-gray-100'
-            }
-          `}
-        >
-          {category}
-        </a>
-      ))}
+      {isExpanded && (
+        <>
+          {categories.map(category => (
+            <a
+              key={category}
+              onClick={() => toggleCategories(category)}
+              className={`px-4 py-2 rounded-full border transition text-sm cursor-pointer
+                ${
+                  selectedCategories.includes(category)
+                    ? 'bg-main text-white border-main'
+                    : 'bg-gray-50 text-gray-700 border-gray-300 hover:bg-gray-100'
+                }
+              `}
+            >
+              {category}
+            </a>
+          ))}
+          <button
+            className="bg-main-light text-main px-4 py-2 rounded-full flex justify-center items-center transform duration-300 hover:opacity-60"
+            onClick={resetCategories}
+          >
+            <RiResetLeftFill />
+          </button>
+        </>
+      )}
+      <button
+        onClick={toggleExpand}
+        className="w-full px-4 py-2 rounded-full flex justify-center items-center text-gray-700 hover:text-main transition"
+      >
+        {isExpanded ? (
+          <p className="flex items-center gap-1 font-dpixel">
+            <span>접기</span>
+            <FaChevronUp />
+          </p>
+        ) : (
+          <p className="flex items-center gap-1 font-dpixel">
+            <span>펼치기</span>
+            <FaChevronDown />
+          </p>
+        )}
+      </button>
     </div>
   );
 }
