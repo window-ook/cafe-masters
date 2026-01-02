81d953937d80a9f92adfa25cada6430c4af470dc
devkoow <cwl64658@gmail.com>
2026-01-02 17:01:33 +0900

    [update] copyright 2024 -> 2026

--- components/landing/FooterSection.tsx
+++ components/landing/FooterSection.tsx
@@ -13,7 +13,7 @@ export default function FooterSection() {
           <p
             className={`text-sm font-medium ${isDarkTheme ? 'text-white' : 'text-text-primary'}`}
           >
-            © 2024 CAFE MASTERS. All rights reserved.
+            © 2026 CAFE MASTERS. All rights reserved.
           </p>
 
           <a
